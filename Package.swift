a4984fa220bbb4322603e56831e50801735236ec
DuongTH.dev <hungduongwp@gmail.com>
2024-07-19 11:32:32 +0700

    update for x

--- Package.swift
+++ Package.swift
@@ -6,18 +6,19 @@ import PackageDescription
 let package = Package(
     name: "SDWebImageSwiftUI",
     platforms: [
-       .macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7)
+        .macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SDWebImageSwiftUI",
-            targets: ["SDWebImageSwiftUI"]),
+            targets: ["SDWebImageSwiftUI"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0")
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
---
+++ SDWebImageSwiftUI/Classes/MultiWebImageManager.swift
@@ -0,0 +1,43 @@
+//
+//  File.swift
+//
+//
+//  Created by Duong To on 7/19/24.
+//
+
+import Foundation
+
+public class MultiWebImageManager: SDWebImageManager {
+    @discardableResult
+    public func loadImage(with url: MultiWebImageURL, options: SDWebImageOptions = [], progress progressBlock: SDImageLoaderProgressBlock?, completed completedBlock: @escaping SDInternalCompletionBlock) -> SDWebImageCombinedOperation? {
+        loadImage(with: url, options: options, context: nil, progress: progressBlock, completed: completedBlock)
+    }
+
+    @discardableResult
+    public func loadImage(with url: MultiWebImageURL, options: SDWebImageOptions = [], context: [SDWebImageContextOption: Any]?, progress progressBlock: SDImageLoaderProgressBlock?, completed completedBlock: @escaping SDInternalCompletionBlock) -> SDWebImageCombinedOperation? {
+        super.loadImage(with: url.primaryUrl, options: options, context: context, progress: progressBlock) { [weak self] image, data, error, cacheType, finished, imageURL in
+
+            guard let self = self else {
+                return
+            }
+            if let error = error as? SDWebImageError, error.code == .cancelled {
+                // Ignore user cancelled
+                // There are race condition when quick scroll
+                // Indicator modifier disapper and trigger `WebImage.body`
+                // So previous View struct call `onDisappear` and cancel the currentOperation
+                return
+            } else if error != nil {
+//                load(url: url?.secondaryURL, options: options, context: context)
+                self.loadImage(with: url.secondaryURL, options: options, context: context, progress: progressBlock, completed: completedBlock)
+                return
+            }
+
+            completedBlock(image, data, error, cacheType, finished, imageURL)
+        }
+    }
+
+    @discardableResult
+    override public func loadImage(with url: URL?, options: SDWebImageOptions = [], context: [SDWebImageContextOption: Any]?, progress progressBlock: SDImageLoaderProgressBlock?, completed completedBlock: @escaping SDInternalCompletionBlock) -> SDWebImageCombinedOperation? {
+        super.loadImage(with: url, options: options, context: context, progress: progressBlock, completed: completedBlock)
+    }
+}
